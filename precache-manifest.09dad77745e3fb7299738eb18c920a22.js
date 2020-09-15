9dc46fa21b2862e1ff0e90a1ec9fef5ee3a1cf46
Surajram.S <surajram1707@gmail.com>
2020-09-15 08:28:24 +0530

    Updates

---
+++ precache-manifest.09dad77745e3fb7299738eb18c920a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac25cd9b599b2a50288c0062b348db3",
+    "url": "/React_Carousel_App/index.html"
+  },
+  {
+    "revision": "4d209ab98df160be3289",
+    "url": "/React_Carousel_App/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "417861edfde4952b1234",
+    "url": "/React_Carousel_App/static/css/main.3426e73e.chunk.css"
+  },
+  {
+    "revision": "4d209ab98df160be3289",
+    "url": "/React_Carousel_App/static/js/2.3b86999c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_Carousel_App/static/js/2.3b86999c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417861edfde4952b1234",
+    "url": "/React_Carousel_App/static/js/main.d183942f.chunk.js"
+  },
+  {
+    "revision": "e03fb394c4ab1b677ffa",
+    "url": "/React_Carousel_App/static/js/runtime-main.e807bd22.js"
+  }
+]);
